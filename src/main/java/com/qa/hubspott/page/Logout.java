e50c00585504ecbfe4351446626e1e248c8e6538
Saikumar <saikumar.patnaikuni@kelltontech.com>
2019-11-26 19:58:26 +0530

    second hand

---
+++ src/main/java/com/qa/hubspott/page/Logout.java
@@ -0,0 +1,11 @@
+
+package com.qa.hubspott.page;
+
+public class Logout {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("Dummy");	
+	}
+
+}
